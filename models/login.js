33017fe9e915ca56bc6d05b8dbdad27b44f26b17
Sahil-late <latesahil67@gmail.com>
2025-11-12 11:09:44 +0530

    some change

--- models/login.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const loginSchema = new mongoose.Schema({
-  username:String,
-  email:String,
-  password:String
-});
-export const Login = mongoose.model('login', loginSchema);
-export default Login;
